95f9f7a76254c9da0f26f60c92030a51202902e4
Raúl <ralo.dev@pm.me>
2023-03-18 19:57:15 -0600

    Update main.js
    • Added libraries to main.js as imports

--- src/main.js
+++ src/main.js
@@ -1,17 +1,29 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import App from './App.vue'
+import axios from 'axios'
 import router from './router'
 
-import './assets/main.css'
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+// Bootstrap - Datatable
+import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'datatables.net-dt/js/dataTables.dataTables'
+import 'datatables.net-dt/css/jquery.dataTables.min.css'
+import 'jquery/dist/jquery.min.js'
+
+library.add(fas)
 
 const app = createApp(App)
-
 app.use(createPinia())
-app.use(router)
-
+app.use(router, axios)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
