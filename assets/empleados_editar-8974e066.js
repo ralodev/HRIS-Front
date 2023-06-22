815465d5ab2a3be45de0edde69dde9d9e74e9925
Raúl <ralo.dev@pm.me>
2023-06-21 18:31:18 -0600

    Update with base path

---
+++ assets/empleados_editar-8974e066.js
@@ -0,0 +1,1 @@
+import{E as e}from"./empleadoForm-d45f63a1.js";import{_ as t,a as r,b as a,j as s}from"./index-cb15b65d.js";import"./empleadoStore-32db8602.js";import"./departamentoStore-14db6ada.js";const m={name:"empleados_editar",components:{EmpleadoForm:e}};function p(n,c,_,d,i,f){const o=r("empleado-form");return a(),s(o,{isEdit:!0})}const E=t(m,[["render",p]]);export{E as default};
