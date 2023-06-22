e234b80258fea849dee6d0085133b6d9e2ef83dd
Raúl <ralo.dev@pm.me>
2023-06-21 18:56:25 -0600

    Update with base removed

--- assets/usuarios_editar-27051311.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./usuarioForm-2249e4f0.js";import{_ as r,a as t,b as a,j as s}from"./index-cb15b65d.js";import"./adminStore-b46cc59e.js";const n={name:"empleados_editar",components:{EmpleadoForm:e}};function m(p,c,_,d,i,f){const o=t("empleado-form");return a(),s(o,{isEdit:!0})}const x=r(n,[["render",m]]);export{x as default};
--- assets/usuarios_registrar-6302e8a3.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./usuarioForm-2249e4f0.js";import{_ as r,a as s,b as a,j as t}from"./index-cb15b65d.js";import"./adminStore-b46cc59e.js";const n={name:"empleados_editar",components:{EmpleadoForm:e}};function m(p,c,_,i,d,f){const o=s("empleado-form");return a(),t(o,{isEdit:!1})}const x=r(n,[["render",m]]);export{x as default};
