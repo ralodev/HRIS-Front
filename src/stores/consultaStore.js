5a789d8669fadebb08efafe147a407e24fb6725a
Raúl <ralo.dev@pm.me>
2023-06-24 13:02:09 -0600

    Update consultaStore.js
    Fixed consulta endpoint

--- src/stores/consultaStore.js
+++ src/stores/consultaStore.js
@@ -2,17 +2,23 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 import { ref } from 'vue'
 
-export const useStore = defineStore('consulta',() => {
+export const useStore = defineStore('consulta', () => {
     //const apiURL = 'http://localhost:8080/api/v1/'
-    const apiURL = 'https://sgip-api-0fe230af7558.herokuapp.com/api/v1/consulta/'
+    const apiURL = 'https://sgip-api-0fe230af7558.herokuapp.com/api/v1/'
     const response = ref({})
     const data = ref([])
     const queryData = ref({})
 
     const getAll = async () => {
-        const response = await axios.get(`${apiURL}consulta/all`)
-        data.value = response.data.data
-        response.value = response
+        try {
+            const response = await axios.get(`${apiURL}consulta/all`)
+            data.value = response.data.data
+            response.value = response
+            return response
+        } catch (err) {
+            console.log(err)
+            return err
+        }
     }
 
     const getUnique = async () => {
