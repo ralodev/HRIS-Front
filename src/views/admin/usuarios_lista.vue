eb00faa01f051ba9039003fd0037dc8aaf3a12ab
Raúl <ralo.dev@pm.me>
2023-06-24 22:16:25 -0600

    Update usuarios_lista.vue
    Admin view of users updated

--- src/views/admin/usuarios_lista.vue
+++ src/views/admin/usuarios_lista.vue
@@ -64,6 +64,7 @@ export default defineComponent({
   components: { DataTable },
   setup() {
     const searchText = ref('');
+    const current_user_email = Cookies.get('email');
     let dt;
     const table = ref();
     //const store = useDataStore();
@@ -76,10 +77,10 @@ export default defineComponent({
       autoWidth: true,
       dom: 'trip',
       paging: true,
-      pagingType: 'simple_numbers',
+      pagingType: 'numbers',
       language: spanish,
       pageLength: 10,
-      order: [[1, 'asc']],
+      order: [[0, 'asc']],
       initComplete: function () {
         this.on('draw.dt', function () {
           nextTick(() => {
@@ -89,43 +90,53 @@ export default defineComponent({
         });
       },
     };
+    /*
+        , render: function (data, row) {
+              let tag;
+              if (data.estado == 'ACTIVO') {
+                tag = "<span class='float-end p-tag p-component p-tag-success' title='Esta cuenta opera normalmente' style='cursor: help !important;'>A<span class='d-lg-block d-none'>ctivo</span></span>"
+              } else if (data.estado == 'BLOQUEADO') {
+                tag = "<span class='float-end p-tag p-component p-tag-danger' title='Este usuario está bloqueado teporalmente' style='cursor: help !important;'>B<span class='d-lg-block d-none'>loqueado</span></span>"
+              } else {
+                tag = "<span class='float-end p-tag p-component p-tag-warning' title='Este usuario no ha confirmado su correo electrónico' style='cursor: help !important;'>P<span class='d-lg-block d-none'>endiente</span>"
+              }
+              return "<span class='not-edited'>" + data.email + "</span>" + ' ' + tag;
+            }
+    */
     const columns = [
       {
-        data: null, title: 'Correo electrónico', class: 'all no-wrap text-nowrap', render: function (data, row) {
+        data: null, title: 'Nombre', class: 'min-tablet-l', width: '20%', render: function (data) {
           let tag;
           if (data.estado == 'ACTIVO') {
-            tag = "<span class='float-end p-tag p-component p-tag-success' title='Esta cuenta opera normalmente' style='cursor: help !important;'>A<span class='d-lg-block d-none'>ctivo</span></span>"
+            tag = "<span class='float-end p-tag p-component p-tag-success' title='Este usuario está habilitado' style='cursor: help !important;'>H<span class='d-lg-block d-none'>abilitado</span></span>"
           } else if (data.estado == 'BLOQUEADO') {
-            tag = "<span class='float-end p-tag p-component p-tag-danger' title='Este usuario está bloqueado teporalmente' style='cursor: help !important;'>B<span class='d-lg-block d-none'>loqueado</span></span>"
+            tag = "<span class='float-end p-tag p-component p-tag-danger' title='Este usuario se encuentra bloqueado' style='cursor: help !important;'>B<span class='d-lg-block d-none'>loqueado</span></span>"
           } else {
             tag = "<span class='float-end p-tag p-component p-tag-warning' title='Este usuario no ha confirmado su correo electrónico' style='cursor: help !important;'>P<span class='d-lg-block d-none'>endiente</span>"
           }
-          return "<span class='not-edited'>" + data.email + "</span>" + ' ' + tag;
+          return data.nombre + " " + data.apellidos + ' ' + tag;
         }
       },
       {
-        data: null, title: 'Nombre', class: 'min-tablet-l', render: function (data) {
-          return data.nombre + " " + data.apellidos;
-        }
-      },
-      {
-        data: 'rol', title: 'Rol', class: 'min-tablet-l', render: function (data) {
+        data: 'rol', title: 'Rol', width: '10%', class: 'min-tablet-l', render: function (data) {
           let num = data == 'ROLE_ADMIN' ? 1 : data == 'ROLE_EMPLEADO_HR' ? 2 : data == 'ROLE_ASISTENTE_HR' ? 3 : 4
           let rol = data.replaceAll("_", " ").replaceAll("ROLE", "");
           return "<span class='d-none'>" + num + "</span>" + rol;
         }
       },
-
       {
-        data: 'estado', class: 'text-nowrap  acciones justify-content-center all ', width: '10%', title: 'Acciones', orderable: false, wrap: true, render: function (data) {
+        data: 'email', title: 'Correo electrónico', class: 'all no-wrap text-nowrap email-col', width: '10%'
+      },
+      {
+        data: null, class: 'text-nowrap  acciones justify-content-center all ', width: '10%', title: 'Acciones', orderable: false, wrap: true, render: function (data) {
           let buttons =
-            '<a title="Ver empleado" class="details-btn me-1 px-3 p-button-sm p-button p-component p-button-icon-only p-button-primary p-button-raised text-decoration-none"><i class="pi pi-user-edit"></i></a> ';
-          if (data == 'BLOQUEADO') {
+          "<a title='Ver empleado' name='"+data.id+"' class='details-btn me-1 px-3 p-button-sm p-button p-component p-button-icon-only p-button-primary p-button-raised text-decoration-none'><i class='pi pi-user-edit'></i></a> ";
+          if (data.estado == 'BLOQUEADO') {
             buttons +=
-              '<a title="Desbloquear" class="unlock-btn px-3 p-button-sm p-button p-component p-button-icon-only p-button-success bg-green-600 p-button-raised text-decoration-none"><i class="pi pi-lock-open"></i></a> ';
+              "<a title='Desbloquear' name='"+data.id+"' class='unlock-btn px-3 p-button-sm p-button p-component p-button-icon-only p-button-success bg-green-600 p-button-raised text-decoration-none'><i class='pi pi-lock-open'></i></a>";
           } else {
             buttons +=
-            '<a title="Bloquear" class="lock-btn px-3 p-button-sm p-button p-component p-button-icon-only p-button-danger p-button-raised text-decoration-none"><i class="pi pi-lock"></i></a> ';
+              "<a title='Bloquear' name='"+data.id+"' class='lock-btn px-3 p-button-sm p-button p-component p-button-icon-only p-button-danger p-button-raised text-decoration-none'><i class='pi pi-lock'></i></a>";
           }
           return buttons;
         },
@@ -173,28 +184,15 @@ export default defineComponent({
       const detailsButt = document.querySelectorAll('.details-btn');
       const lockButt = document.querySelectorAll('.lock-btn');
       const unlockButt = document.querySelectorAll('.unlock-btn');
+      const email_cols = document.querySelectorAll('.email-col');
       const email = Cookies.get('email');
 
       detailsButt.forEach((butt) => {
-        if (butt.parentElement.parentElement.firstChild.innerText.split('\n')[0] == email){
-          butt.nextElementSibling.classList.add('p-disabled');
-          butt.nextElementSibling.classList.remove('p-button-danger');
-          butt.nextElementSibling.classList.add('p-button-secondary');
-          butt.nextElementSibling.title = 'No puedes bloquear tu propia cuenta';
-          butt.classList.add('p-disabled');
-          butt.classList.remove('p-button-primary');
-          butt.classList.add('p-button-secondary');
-          butt.title = 'No puedes editar tu propia cuenta';
-          if (butt.parentElement.parentElement.firstChild.firstChild.classList.contains('not-edited')){
-            butt.parentElement.parentElement.firstChild.innerHTML += "<span class='float-end me-1 p-tag p-component p-tag-info' title='Esta es tu cuenta' style='cursor: help !important;'>Tú</span>"
-            butt.parentElement.parentElement.firstChild.firstChild.classList.remove('not-edited');
-          }
-        }
         butt.parentElement.classList.add('flex');
         butt.addEventListener('click', () => {
           router.push({
             name: 'usuarios_editar', params: {
-              id: butt.closest('tr').cells[0].firstChild.innerHTML
+              id: butt.attributes.name.value
             }
           });
         });
@@ -202,14 +200,26 @@ export default defineComponent({
       lockButt.forEach((butt) => {
         butt.parentElement.classList.add('flex');
         butt.addEventListener('click', () => {
-          bloquearUsuario(butt.closest('tr').cells[0].innerText.split('\n')[0]);
+          bloquearUsuario(butt.attributes.name.value);
         });
       });
       unlockButt.forEach((butt) => {
         butt.parentElement.classList.add('flex');
         butt.addEventListener('click', () => {
-          desbloquearUsuario(butt.closest('tr').cells[0].innerText.split('\n')[0]);
+          desbloquearUsuario(butt.attributes.name.value);
         });
+      });
+      email_cols.forEach((email_col) => {
+        if (email_col.innerText == email) {
+          email_col.closest('tr').children[3].title = 'No puedes editar tu propio usuario';
+          let detail_button = email_col.closest('tr').children[3].children[0];
+          detail_button.classList.add('p-disabled');
+          detail_button.classList.add('p-button-secondary');
+          detail_button.classList.remove('p-button-primary');
+          detail_button.nextElementSibling.classList.add('p-disabled');
+          detail_button.nextElementSibling.classList.add('p-button-secondary');
+          detail_button.nextElementSibling.classList.remove('p-button-danger');
+        }
       });
     };
 
@@ -287,4 +297,5 @@ tr td a {
   height: 35px !important;
   width: 20px !important;
   box-shadow: inset 1px 1px 3px 0px #ffffff7a !important;
-}</style>
+}
+</style>
